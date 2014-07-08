69500a3e0ebc18a2f309cfd84587be3823ff71aa
Conor <conorpp@vt.edu>
2014-07-07 23:04:31 -0400

    added settting structure

--- src/proxy.h
+++ src/proxy.h
@@ -6,11 +6,42 @@
 #include <string.h>
 #include <zlib.h>
 #include "utils.h"
+#include "regex.h"
 
 
 
-/* Functionaly specific to proxying
+/* Functionaly specific to application and proxying
 * */
+
+struct __SETTINGS__{
+    struct{
+        uchar gravity;
+        uchar rickroll;
+    }events;
+    struct{
+        int position;
+        uchar findTag;
+        uchar findAttr;
+        uchar saveClient;
+        uchar saveServer;
+        uchar saveHeaders;
+        long long int count;
+    }options;
+    struct{
+        uchar enabled;
+        char* certfile;
+        char* privfile;
+    }ssl;
+    char* targetHost;
+    char* regexString;
+    Regex* regex;
+    char* replaceString;
+    char** files;
+    int filenum;
+    char* port;
+}Prox;
+
+
 
 // Decodes a deflate or gzip buffer.
 ///@param gzip: the compressed buffer
